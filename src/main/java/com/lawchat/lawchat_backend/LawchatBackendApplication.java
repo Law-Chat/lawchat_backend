375ab658e79291fb6976975003d9fb1ae4e06e0a
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-11-04 12:16:41 +0900

    init: 초기 세팅

---
+++ src/main/java/com/lawchat/lawchat_backend/LawchatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lawchat.lawchat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawchatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawchatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lawchat/lawchat_backend/LawchatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawchat.lawchat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawchatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
